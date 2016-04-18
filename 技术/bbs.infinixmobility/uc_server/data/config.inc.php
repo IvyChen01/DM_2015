1f0585ff439f1ed38e28aba996e6629febe6f818
IvyChen01 <ivy.chen@transsion.com>
2016-04-18 17:44:39 +0800

    Update config.inc.php

--- 技术/bbs.infinixmobility/uc_server/data/config.inc.php
+++ 技术/bbs.infinixmobility/uc_server/data/config.inc.php
@@ -1,19 +1,19 @@
 <?php 
-define('UC_DBHOST', '127.0.0.1');
-define('UC_DBUSER', 'root');
-define('UC_DBPW', 'DB69transsion');
-define('UC_DBNAME', 'infinixbbs');
-define('UC_DBCHARSET', 'utf8');
-define('UC_DBTABLEPRE', 'infinixbbs_ucenter_');
+define('UC_DBHOST', '');
+define('UC_DBUSER', '');
+define('UC_DBPW', '');
+define('UC_DBNAME', '');
+define('UC_DBCHARSET', '');
+define('UC_DBTABLEPRE', '_');
 define('UC_COOKIEPATH', '/');
 define('UC_COOKIEDOMAIN', '');
 define('UC_DBCONNECT', 0);
 define('UC_CHARSET', 'utf-8');
-define('UC_FOUNDERPW', '312d23469e5986dfc80a404e44206e7f');
+define('UC_FOUNDERPW', '3');
 define('UC_FOUNDERSALT', '111162');
-define('UC_KEY', 'sfRbAcl2oaqav77f69C6yaI4wbx3ifvfA3bev7x75964teLaZ0I8b0H867mdp647');
-define('UC_SITEID', '0fhbncb2laGa27Qfg9D61aC44bo3FfIf53dei7v7m9b4OeyaK008d0D8Y7Gdi6s7');
-define('UC_MYKEY', 'VfQbQc720aka97Xfy906Kax4cbM3Sf9fs3sev7j7x9u46eNaf0m8p0O817ndO6l7');
+define('UC_KEY', '');
+define('UC_SITEID', '0fhbncb2laGa27Qfg9D61aC44bo3FfIf53dei7v7m9b4eyaK008d0D8Y7G7');
+define('UC_MYKEY', '');
 define('UC_DEBUG', false);
 define('UC_PPP', 20);
 
