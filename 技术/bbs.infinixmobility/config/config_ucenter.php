fb4a698cefccb711eecc758aaf3238e7c9bc9f5b
IvyChen01 <ivy.chen@transsion.com>
2016-04-18 14:56:07 +0800

    Update config_ucenter.php

--- 技术/bbs.infinixmobility/config/config_ucenter.php
+++ 技术/bbs.infinixmobility/config/config_ucenter.php
@@ -1,19 +1,19 @@
 <?php
 
 
-define('UC_CONNECT', 'mysql');
+define('UC_CONNECT', '');
 
-define('UC_DBHOST', '127.0.0.1');
-define('UC_DBUSER', 'root');
-define('UC_DBPW', 'DB69transsion');
-define('UC_DBNAME', 'infinixbbs');
-define('UC_DBCHARSET', 'utf8');
-define('UC_DBTABLEPRE', '`infinixbbs`.infinixbbs_ucenter_');
+define('UC_DBHOST', '');
+define('UC_DBUSER', '');
+define('UC_DBPW', '');
+define('UC_DBNAME', '');
+define('UC_DBCHARSET', '');
+define('UC_DBTABLEPRE', '``.');
 define('UC_DBCONNECT', 0);
 
 define('UC_CHARSET', 'utf-8');
-define('UC_KEY', '4fkbHcj2faqau78ff9Y66aQ4vb53sfFfh3Veg7J7s9c4Meva30S800B8v7fdN6u7');
-define('UC_API', 'http://bbs.infinixmobility.com/uc_server');
+define('UC_KEY', '');
+define('UC_API', '');
 define('UC_APPID', '1');
 define('UC_IP', '');
 define('UC_PPP', 20);
