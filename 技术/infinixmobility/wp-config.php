d8ebdc417cd98fcf3c6fed15713e8961d86b677e
IvyChen01 <ivy.chen@transsion.com>
2016-04-18 14:57:24 +0800

    Update wp-config.php

--- 技术/infinixmobility/wp-config.php
+++ 技术/infinixmobility/wp-config.php
@@ -16,19 +16,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'infinix');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'DB69transsion');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', '');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
